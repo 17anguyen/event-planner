76485857966facdce13aeb565ad9f4bc8adb9ffc
Carolina Haggerty <work@Carolinas-MacBook-Air-1473.local>
2023-04-11 09:57:30 -0700

    working on Api

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,4 @@
-var inputEl = document.getElementById('datepicker');
-var dateSelected = '';
-
-
-function loadPicker(){
-     var instances = M.Datepicker.init(inputEl);
-     var int = M.Datepicker.getInstance(inputEl)
-    
-    console.log(int.$el[0].M_Datepicker.Datepicker)
-
-    //  console.log(instances[0].$el[0].M_Datepicker)
-    //  console.log(instances[0].$el[0].M_Datepicker['date'])       
-
-}
-
-loadPicker();
+console.log(window.location.origin)
+console.log(window.location.host)
+console.log(window.location.hostname)
+console.log(window.location.port)
